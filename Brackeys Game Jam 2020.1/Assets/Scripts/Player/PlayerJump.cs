07d9b0f7043b8937bbd737d2ddcd8e8c21c4216f
Anantha Krishna R <sujakrishnar@gmail.com>
2020-02-16 23:45:49 +0530

    Corrected double jump problem.

--- Brackeys Game Jam 2020.1/Assets/Scripts/Player/PlayerJump.cs
+++ Brackeys Game Jam 2020.1/Assets/Scripts/Player/PlayerJump.cs
@@ -8,17 +8,25 @@ public class PlayerOptimisedJump : MonoBehaviour
     public float jumpVelocity = 6f;
     public LayerMask groundLayer;
     
+    bool prevGrounded = false;
+    
     void Update()
     {
-        if (Input.GetButtonDown("Jump") && IsGrounded())
+        if (Input.GetButtonDown("Jump") && IsGrounded() && prevGrounded)
         {
             GetComponent<Rigidbody2D>().velocity = Vector2.up * jumpVelocity;
+        }
+        else
+        {
+            prevGrounded = false;
         }
     }
     
     //Copied from Assets/Scripts/PlayerScript.cs as temporary fix.
     private bool IsGrounded()
     {
+        prevGrounded = true;
+        
         RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, Vector2.down, 1.3f, groundLayer);
         if (hitInfo.collider != null)
         {
