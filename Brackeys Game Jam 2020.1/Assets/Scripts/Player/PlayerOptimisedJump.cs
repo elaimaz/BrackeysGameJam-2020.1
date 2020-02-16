1575cf7e1c93bc56b823b9b2f1f45a93e5f1dc76
Anantha Krishna R <sujakrishnar@gmail.com>
2020-02-16 22:34:20 +0530

    Fix #35 Wrote working movement and jump and applied.
    Side Note: i unchecked (deactivate) the existing script on player to make sure the movement works.
    Added my scripts under Assets/Player folder.

---
+++ Brackeys Game Jam 2020.1/Assets/Scripts/Player/PlayerOptimisedJump.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerOptimisedJump : MonoBehaviour
+{
+    [Range(1, 10)]
+    public float jumpVelocity;
+    
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            GetComponent<Rigidbody2D>().velocity = Vector2.up * jumpVelocity;
+        }
+    }
+}
