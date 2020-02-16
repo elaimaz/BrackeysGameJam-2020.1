ab24d9831a9b5d25dbc3a7722190d0b874872498
Anantha Krishna R <sujakrishnar@gmail.com>
2020-02-17 00:14:59 +0530

    Fix #38 Removed rigid body dependency of PlayerWalk script

--- Brackeys Game Jam 2020.1/Assets/Scripts/Player/PlayerWalk.cs
+++ Brackeys Game Jam 2020.1/Assets/Scripts/Player/PlayerWalk.cs
@@ -6,8 +6,6 @@ public class PlayerWalk : MonoBehaviour
 {
     public float moveSpeed = 3f;
     
-    public Rigidbody2D rb;
-    
     Vector3 movement;
     Vector2 axis;
     // Update is called once per frame
@@ -16,15 +14,5 @@ public class PlayerWalk : MonoBehaviour
         movement.x = axis.x = Input.GetAxisRaw("Horizontal");
         
         transform.position += movement * moveSpeed * Time.deltaTime;
-        
-        
-        //comment next line and uncomment lines in fixed update to move movement to fixedUpdate.
-////        if (movement.x != 0)
-////            rb.MovePosition(rb.position + movement * moveSpeed * Time.deltaTime);
-    }
-    
-    void FixedUpdate()
-    {
-//        if (!movement.x == 0) rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);      
     }
 }
