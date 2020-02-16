21026713b131214c183ac58d77a3ae0b5cb181f9
Anantha Krishna R <sujakrishnar@gmail.com>
2020-02-16 23:24:41 +0530

    Fix #38 Added GroundLayer and properly implemented jumping.
    Added GroundLayer where we can set out ground tiles to that.
    Fixed flying.
    Fixed some coding errors and missing implementation.

--- Brackeys Game Jam 2020.1/Assets/Scripts/Player/PlayerJump.cs
+++ Brackeys Game Jam 2020.1/Assets/Scripts/Player/PlayerJump.cs
@@ -6,12 +6,24 @@ public class PlayerOptimisedJump : MonoBehaviour
 {
     [Range(1, 10)]
     public float jumpVelocity = 6f;
+    public LayerMask groundLayer;
     
     void Update()
     {
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && IsGrounded())
         {
             GetComponent<Rigidbody2D>().velocity = Vector2.up * jumpVelocity;
         }
+    }
+    
+    //Copied from Assets/Scripts/PlayerScript.cs as temporary fix.
+    private bool IsGrounded()
+    {
+        RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, Vector2.down, 1.3f, groundLayer);
+        if (hitInfo.collider != null)
+        {
+            return true;
+        }
+        return false;
     }
 }
