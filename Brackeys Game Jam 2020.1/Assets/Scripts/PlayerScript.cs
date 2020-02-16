f4bc75989d3beec05bced9322528e11ffeae77f8
Wesley <fariawesley4@gmail.com>
2020-02-16 09:59:14 -0300

    Fix #11 Create Player Walk/Run
    Simple Player Left Right movement created using rigid body.
    There is a lot of archives going on to master, but it is because i'm using it to test, also i created my own scene to make tests.

---
+++ Brackeys Game Jam 2020.1/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    [SerializeField]
+    private float playerSpeed = 5;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        Movement();
+    }
+
+    private void Movement()
+    {
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(horizontalInput * playerSpeed, rb.velocity.y);
+    }
+}
