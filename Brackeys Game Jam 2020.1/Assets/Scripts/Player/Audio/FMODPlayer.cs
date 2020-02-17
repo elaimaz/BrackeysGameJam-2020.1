a28d662043379fbbda1771fa7cce6164d78de84e
Jeremy <jezjez@gmail.com>
2020-02-17 23:00:36 +0900

    Add footsteps fx and link to player animation

---
+++ Brackeys Game Jam 2020.1/Assets/Scripts/Player/Audio/FMODPlayer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FMODPlayer : MonoBehaviour
+{
+    private float distance = 0.1f;
+
+    void PlayFootsteps(string path)
+    {
+        FMOD.Studio.EventInstance Footsteps = FMODUnity.RuntimeManager.CreateInstance(path);
+        Footsteps.start();
+        Footsteps.release();
+    }
+}
