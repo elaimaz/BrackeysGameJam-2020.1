577d08d4165aff5ebf0bb394847ddecf4dd0091e
Wesley <fariawesley4@gmail.com>
2020-02-18 19:30:06 -0300

    Fix #150 Setup Blob Walk/Run Animation

--- Brackeys Game Jam 2020.1/Assets/Scripts/Enemy/Blob/BlobTests.cs
+++ Brackeys Game Jam 2020.1/Assets/Scripts/Enemy/Blob/BlobTests.cs
@@ -22,5 +22,8 @@ public class BlobTests : MonoBehaviour
             count++;
             animator.SetTrigger("Death");
         }
+
+        float horizontalINput = Input.GetAxis("Horizontal");
+        animator.SetFloat("Move", Mathf.Abs(horizontalINput));
     }
 }
