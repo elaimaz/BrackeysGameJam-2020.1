fac02891d55a8aab68ec47520e7a3ebfdf5bd1fa
Anantha Krishna R <sujakrishnar@gmail.com>
2020-02-18 23:43:38 +0530

    Early Commit

---
+++ Brackeys Game Jam 2020.1/Assets/Scripts/HUDScripts/PlayerHealthBar.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealthBar : MonoBehaviour
+{
+    public GameObject healthBar;
+    
+    
+}
---
+++ Brackeys Game Jam 2020.1/Assets/Scripts/HUDScripts/PowerUpBar.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PowerUpBar : MonoBehaviour
+{
+
+    public Slider slider;
+    public Gradient gradient;
+    public Image fill;
+    
+    public void Start(){
+        fill.color = gradient.Evaluate(slider.value / slider.normalizedValue);
+    }
+    
+    public void SetMaxPoints(int points){
+    
+        slider.maxValue = points;
+        
+        fill.color = gradient.Evaluate(slider.value / slider.normalizedValue);
+    }
+    
+    public void SetPoints(int points){
+        slider.value = points;
+        
+        fill.color = gradient.Evaluate(slider.value / slider.normalizedValue);
+    }
+    
+    public void AddPoints(int points){
+        if ((slider.value + points)> slider.maxValue){
+            slider.value = slider.maxValue;
+        }
+        else slider.value += points;
+        
+        fill.color = gradient.Evaluate(slider.value / slider.normalizedValue);
+    }
+    
+    public void RemovePoints(int points){
+        if ((slider.value - points) <= 0){
+            slider.value = 0;
+        }
+        else slider.value -= points;
+        
+        fill.color = gradient.Evaluate(slider.value / slider.normalizedValue);
+    }
+    
+    public void ResetToZero(){
+        slider.value = 0;
+        fill.color = gradient.Evaluate(slider.value / slider.normalizedValue);
+    }
+}
