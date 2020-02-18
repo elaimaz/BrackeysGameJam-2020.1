e4bb300a078f9a492b4a45f82d6b52bf7ff1206f
Jeremy <jezjez@gmail.com>
2020-02-18 15:47:02 +0900

    add portal sound

--- Brackeys Game Jam 2020.1/Assets/Scripts/Player/PlayerController.cs
+++ Brackeys Game Jam 2020.1/Assets/Scripts/Player/PlayerController.cs
@@ -55,6 +55,7 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.E))
         {
             playerAnimator.JumpPortal();
+            FMODUnity.RuntimeManager.PlayOneShot("event:/FX/Portal");
         }
     }
 
