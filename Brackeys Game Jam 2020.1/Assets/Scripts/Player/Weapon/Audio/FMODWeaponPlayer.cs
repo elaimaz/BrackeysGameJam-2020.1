90b801c1ee9d4d275ff5f6ef730279292651a9ba
Jeremy <jezjez@gmail.com>
2020-02-18 14:00:23 +0900

    create melee drill attack sound

---
+++ Brackeys Game Jam 2020.1/Assets/Scripts/Player/Weapon/Audio/FMODWeaponPlayer.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FMODWeaponPlayer : MonoBehaviour
+{
+    void PlayMelee(string path)
+    {
+        FMOD.Studio.EventInstance Melee = FMODUnity.RuntimeManager.CreateInstance(path);
+        Melee.start();
+        Melee.release();
+    }
+}
