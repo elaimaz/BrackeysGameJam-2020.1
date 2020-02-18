6964b3b0228c801568656e91529a0b538b2d6893
Jeremy <jezjez@gmail.com>
2020-02-18 23:38:00 +0900

    Add ranged weapon sound

--- Brackeys Game Jam 2020.1/Assets/Scripts/Player/Weapon/Audio/FMODMeleeWeaponPlayer.cs
+++ Brackeys Game Jam 2020.1/Assets/Scripts/Player/Weapon/Audio/FMODMeleeWeaponPlayer.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FMODWeaponPlayer : MonoBehaviour
+public class FMODMeleeWeaponPlayer : MonoBehaviour
 {
     void PlayMelee(string path)
     {
---
+++ Brackeys Game Jam 2020.1/Assets/Scripts/Player/Weapon/Audio/FMODRangedWeaponPlayer.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FMODRangedWeaponPlayer : MonoBehaviour
+{
+    void PlayRanged(string path)
+    {
+        FMOD.Studio.EventInstance PlayRanged = FMODUnity.RuntimeManager.CreateInstance(path);
+        PlayRanged.start();
+        PlayRanged.release();
+    }
+}
