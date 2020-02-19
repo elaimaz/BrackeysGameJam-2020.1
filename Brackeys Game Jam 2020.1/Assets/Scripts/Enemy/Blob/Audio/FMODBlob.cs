206d70140c64d748519d9954e5abae2ee6d39d27
Jeremy <jezjez@gmail.com>
2020-02-19 11:53:13 +0900

    add blob walk sound

---
+++ Brackeys Game Jam 2020.1/Assets/Scripts/Enemy/Blob/Audio/FMODBlob.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FMODBlob : MonoBehaviour
+{
+    void PlayFootsteps(string path)
+    {
+        FMOD.Studio.EventInstance Footsteps = FMODUnity.RuntimeManager.CreateInstance(path);
+        Footsteps.start();
+        Footsteps.release();
+    }
+}
