dbf05f4e8532eb81620f860a4183dd0d4e3c8799
Wesley <fariawesley4@gmail.com>
2020-02-22 02:11:51 -0300

    Fix #310 Create Gate Mechanic

---
+++ Brackeys Game Jam 2020.1/Assets/Scripts/Gate/CloseGateInstant.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloseGateInstant : MonoBehaviour
+{
+    public GameObject openGate;
+    public GameObject closeGate;
+
+    public GameObject invisibleWall;
+    
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            openGate.SetActive(false);
+            closeGate.SetActive(true);
+            invisibleWall.SetActive(true);
+        }
+
+    }
+}
