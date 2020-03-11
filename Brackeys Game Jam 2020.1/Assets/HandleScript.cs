08953dc41ad7d5514ed404f64c64f98ac1ebe21e
Anantha Krishna R <sujakrishnar@gmail.com>
2020-03-11 21:27:52 +0530

    Player now can move properly

---
+++ Brackeys Game Jam 2020.1/Assets/HandleScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandleScript : MonoBehaviour
+{
+    
+    public float Horizontal = 0;
+    public float Vertical = 0;
+    
+    public Joystick joystick;
+    public GameObject joystickParent;
+    // Update is called once per frame
+    void Update()
+    {
+        
+        
+        Vector2 pos = transform.position - joystickParent.transform.position;
+        
+        print("x = " + pos.x + "  y = " + pos.y);
+        if ((pos.x == 0) && (pos.y == 0)){
+            Horizontal = 0;
+            Vertical = 0;
+        }
+        else {
+            Horizontal = joystick.Horizontal;
+            Vertical = joystick.Vertical;
+        }
+    }
+}
